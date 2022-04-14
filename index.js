799d08f51a242049388100fea5b8295944dc9268
Shamsath Maleha Anika <17203020@iubat.edu>
2022-04-15 03:32:03 +0600

    op

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ admin.initializeApp({
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ucxei.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+// 
 // 
 
 async function verifyToken(req, res, next) {
